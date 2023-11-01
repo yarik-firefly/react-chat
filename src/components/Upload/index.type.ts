e8b364df72ff5cb3acd93da56d462090b2422fff
yarik-firefly <snowboardvgostahu@gmail.com>
2023-11-02 01:19:56 +0200

    Полностью пернес проэкт на Typescript, на лок хосте почему то падает сервер

--- src/components/Upload/index.type.ts
+++ src/components/Upload/index.type.ts
@@ -1,10 +1,10 @@
 import { UploadFile } from "antd";
-import { IAttachments } from "../Input/Input.type";
+import { TypeAttachments } from "../Input/Input.type";
 import { UploadChangeParam } from "antd/es/upload";
 import { ISetState } from "../Input/Input.type";
 
 export interface IUploadImage {
-  fileList: UploadFile<IAttachments>[];
+  fileList: UploadFile<TypeAttachments>[];
   handlePreview: (file: UploadFile) => Promise<void>;
   handleChange:
     | ((info: UploadChangeParam<UploadFile<any>>) => void)
